683211c3aa2ffa2bb93c4e434979ecb6b7e0c6bc
camperotactico <contact@camperotacti.co>
2025-09-24 18:56:02 +0200

    WIP new tool to create custom types.

--- addons/resource_based_signal_bus/resource_based_signal_bus_plugin.gd
+++ addons/resource_based_signal_bus/resource_based_signal_bus_plugin.gd
@@ -1,17 +1,26 @@
 @tool
 extends EditorPlugin
 
+# Signal Bus Listener
 const SIGNAL_BUS_LISTENER: String = "SignalBusListener"
 const SignalBusListenerInspectorTool = preload("uid://bgyam130l63xn")
 const SignalBusListener = preload("uid://da6dhfdepfkku")
 const SIGNAL_BUS_LISTENER_ICON = preload("uid://dkfpyxcv5des0")
-
 var signal_bus_listener_inspector_tool: SignalBusListenerInspectorTool
 
+# Custom Signal Bus Creator
+const CustomSignalBusScriptCreator = preload("uid://of5af3ng1gf0")
+var custom_signal_bus_script_creator: Control
+
 func _enter_tree() -> void:
+	# Signal Bus Listener
 	signal_bus_listener_inspector_tool = SignalBusListenerInspectorTool.new()
 	add_inspector_plugin(signal_bus_listener_inspector_tool)
 	add_custom_type(SIGNAL_BUS_LISTENER,"Node",SignalBusListener,SIGNAL_BUS_LISTENER_ICON)
+	
+	# Custom Signal Bus Creator
+	custom_signal_bus_script_creator = CustomSignalBusScriptCreator.instantiate()
+	add_control_to_dock(DOCK_SLOT_LEFT_UR,custom_signal_bus_script_creator)
 
 
 func _enable_plugin() -> void:
@@ -25,5 +34,11 @@ func _disable_plugin() -> void:
 
 
 func _exit_tree() -> void:
+	# Signal Bus Listener
 	remove_inspector_plugin(signal_bus_listener_inspector_tool)
 	remove_custom_type(SIGNAL_BUS_LISTENER)
+	
+	# Custom Signal Bus Creator
+	remove_control_from_docks(custom_signal_bus_script_creator)
+	custom_signal_bus_script_creator.free()
+	
---
+++ addons/resource_based_signal_bus/scripts/custom_signal_bus_script_creator/custom_signal_bus_script_creator.gd
@@ -0,0 +1,112 @@
+@tool
+extends Control
+
+const ERROR_TEMPLATE: String = "[color=red]%s[/color]"
+
+# Save Path Validation
+const CHARACTERS_NOT_ALLOWED_IN_PATH = ':\\?*"|%<>'
+const ABSOLUTE_RES_PATH: String = "res://"
+const GD_EXTENSION: String = ".gd"
+
+signal preview_filename_changed(new_preview_filename: String)
+signal error_encountered(error_message: String)
+signal script_creation_disabled(is_disabled: bool)
+
+
+@export var class_name_line_edit: LineEdit
+@export var save_path_line_edit: LineEdit
+
+var _class_name: String = ""
+var _save_path: String = ""
+
+var _file_system: EditorFileSystem
+
+func _enter_tree() -> void:
+	_file_system = EditorInterface.get_resource_filesystem()
+	_file_system.filesystem_changed.connect(_on_filesystem_changed)
+	class_name_line_edit.text_changed.connect(_on_class_name_line_edit_text_changed)
+	save_path_line_edit.text_changed.connect(_on_save_path_line_edit_text_changed)
+	
+	_on_class_name_line_edit_text_changed(class_name_line_edit.text)
+	_on_save_path_line_edit_text_changed(save_path_line_edit.text)
+	_validate_parameters()
+
+func _exit_tree() -> void:
+	class_name_line_edit.text_changed.disconnect(_on_class_name_line_edit_text_changed)
+	save_path_line_edit.text_changed.disconnect(_on_save_path_line_edit_text_changed)
+	
+	_file_system.filesystem_changed.disconnect(_on_filesystem_changed)
+	_file_system = null
+
+func _on_filesystem_changed() -> void:
+	_validate_parameters()
+
+func _try_file_system_refresh() -> void:
+	if not _file_system.is_scanning():
+		_file_system.scan()
+
+func _on_create_script_button_pressed() -> void:
+	save_script_to_file()
+
+func save_script_to_file():
+	if !DirAccess.dir_exists_absolute(_save_path):
+		DirAccess.make_dir_recursive_absolute(_save_path)
+	
+	var file = FileAccess.open(_get_filepath(), FileAccess.WRITE)
+	if !file:
+		print(error_string(FileAccess.get_open_error()))
+		return
+	file.store_line("@tool")
+	file.store_line("extends SignalBus")
+	file.store_line("")
+	file.store_string("class_name %s" % _class_name)
+	file.close()
+	
+	# Refresh the FileSystem
+	_try_file_system_refresh()
+
+func _on_class_name_line_edit_text_changed(new_text: String) -> void:
+	_class_name = new_text
+	preview_filename_changed.emit(_class_name.to_snake_case()+GD_EXTENSION)
+	_validate_parameters()
+
+func _on_save_path_line_edit_text_changed(new_text: String) -> void:
+	_save_path = new_text
+	_validate_parameters()
+
+func _validate_parameters() -> void:
+	if _are_parameters_valid():
+		script_creation_disabled.emit(false)
+	else:
+		script_creation_disabled.emit(true)
+		
+func _are_parameters_valid() -> bool:
+	# Check save path
+	if _save_path.is_empty():
+		_display_error("Save Path cannot be empty")
+		return false
+
+	var basename_save_path = _save_path.get_basename()
+	if basename_save_path.is_empty() or !basename_save_path.begins_with(ABSOLUTE_RES_PATH):
+		_display_error("Save Path not valid. It should follow the pattern: [i]res://path/to/scripts[/i]")
+		return false
+	
+	var substr_basename_save_path = basename_save_path.substr(ABSOLUTE_RES_PATH.length())
+	for character_not_allowed_in_path in CHARACTERS_NOT_ALLOWED_IN_PATH:
+		if character_not_allowed_in_path in substr_basename_save_path:
+			_display_error("Save Path cannot contain the character '%s'" % character_not_allowed_in_path)
+			return false
+	
+	# Check class name / filename
+	
+	# Check for conflicting files
+	if FileAccess.file_exists(_get_filepath()):
+		_display_error("A file with the same name already exists on that path")
+		return false
+	return true
+
+func _display_error(error_message: String) -> void:
+	error_encountered.emit(ERROR_TEMPLATE % error_message)
+
+func _get_filepath() -> String:
+	return _save_path.get_basename().path_join(_class_name.to_snake_case()+GD_EXTENSION)
